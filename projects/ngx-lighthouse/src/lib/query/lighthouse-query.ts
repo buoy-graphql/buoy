427f784f6c2e5f2844b98ca8c05949b3d7e295ea
Albert Haff <ahn@haffdata.com>
2019-02-17 23:12:30 +0100

    Removed old files

--- projects/ngx-lighthouse/src/lib/query/lighthouse-query.ts
+++
@@ -1,5 +0,0 @@
-export class LighthouseQuery {
-    constructor() {
-
-    }
-}
--- projects/ngx-lighthouse/src/public_api.ts
+++
@@ -1,6 +0,0 @@
-/*
- * Public API Surface of ngx-lighthouse
- */
-
-export * from './lib/http-link/lighthouse-link';
-export * from './lib/http-link/lighthouse-link-options';
