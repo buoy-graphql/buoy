b53a421b9803a439cc09a6ca75562a4d7a18fe21
Albert Haff <ahn@haffdata.com>
2019-02-19 12:57:23 +0100

    Updated footer

--- website/core/Footer.js
+++ website/core/Footer.js
@@ -17,57 +17,56 @@ class Footer extends React.Component {
     render() {
         return (
             <footer className="nav-footer" id="footer">
-            <section className="sitemap">
-            <a href={this.props.config.baseUrl} className="nav-home">
-            {this.props.config.footerIcon && (
-            <img
-        src={this.props.config.baseUrl + this.props.config.footerIcon}
-        alt={this.props.config.title}
-        width="66"
-        height="58"
-            />
-    )}
-    </a>
-        <div>
-        <h5>Documentation</h5>
-        <a href={this.docUrl('getting-started/installation')}>
-        Getting Started
-        </a>
-        <a href={this.docUrl('api-reference/lighthouse')}>
-        API Reference
-        </a>
-        </div>
-        <div>
-        <h5>Community</h5>
-        <a href={this.pageUrl('users')}>
-        User Showcase
-        </a>
-        <a
-        href="http://stackoverflow.com/questions/tagged/ngx-buoy"
-        target="_blank"
-        rel="noreferrer noopener">
-            Stack Overflow
-        </a>
-        </div>
-        <div>
-        <h5>More</h5>
-        <a target="new" href={this.props.config.repoUrl}>GitHub</a>
-        <a target="new" href={'https://www.npmjs.com/package/' + this.props.config.npmPackage}>NPM</a>
-        <a href={this.props.config.repoUrl}
-        className="github-button"
-        data-icon="octicon-star"
-        data-count-href="/alberthaff/ngx-buoy/stargazers"
-        data-show-count="true"
-        data-count-aria-label="# stargazers on GitHub"
-        aria-label="Star this project on GitHub">
-            Star
-            </a>
-            </div>
-            </section>
+                <section className="sitemap" style={{paddingLeft: '250px'}}>
+                    <a href={this.props.config.baseUrl}>
+                    {this.props.config.footerIcon && (
+                        <img src={this.props.config.baseUrl + this.props.config.footerIcon}
+                             style={{position: 'absolute', marginLeft: '-250px'}}
+                             alt={this.props.config.title}
+                             width="190"
+                             height="190"
+                        />
+                    )}
+                    </a>
+                    <div>
+                        <h5>Documentation</h5>
+                        <a href={this.docUrl('getting-started/installation')}>
+                            Getting Started
+                        </a>
+                        <a href={this.docUrl('api-reference/lighthouse')}>
+                            API Reference
+                        </a>
+                    </div>
+                    <div>
+                        <h5>Community</h5>
+                        <a href={this.pageUrl('users')}>
+                            User Showcase
+                        </a>
+                        <a href="http://stackoverflow.com/questions/tagged/ngx-buoy"
+                           target="_blank"
+                           rel="noreferrer noopener">
+                            Stack Overflow
+                        </a>
+                    </div>
+                    <div>
+                        <h5>More</h5>
+                        <a target="new" href={this.props.config.repoUrl}>GitHub</a>
+                        <a target="new" href={'https://www.npmjs.com/package/' + this.props.config.npmPackage}>NPM</a>
+                        <a href={this.props.config.repoUrl}
+                        className="github-button"
+                        data-icon="octicon-star"
+                        data-count-href="/alberthaff/ngx-buoy/stargazers"
+                        data-show-count="true"
+                        data-count-aria-label="# stargazers on GitHub"
+                        aria-label="Star this project on GitHub">
+                            Star
+                            </a>
+                    </div>
+                </section>
 
-            <section className="copyright">{this.props.config.copyright}</section>
-        </footer>
-    );
+                <section className="copyright">{this.props.config.copyright}</section>
+            </footer>
+        );
     }
 }
 
