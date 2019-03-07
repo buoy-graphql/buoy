46daff38ef0da22673242a2f393c285b22272a7d
Albert Haff <ahn@haffdata.com>
2019-03-07 15:17:38 +0100

    Updated example

--- example/src/app/app.module.ts
+++ example/src/app/app.module.ts
@@ -4,8 +4,8 @@ import {NgModule} from '@angular/core';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {environment} from '../environments/environment';
-import {BuoyConfig} from '../../../ngx-buoy/src/lib/buoy-config';
-import {BuoyModule} from '../../../ngx-buoy/src/lib/buoy.module';
+import {BuoyConfig} from '../../../src/lib/buoy-config';
+import {BuoyModule} from '../../../src/lib/buoy.module';
 
 const BuoyConfigValue = <BuoyConfig>{
     endpoint: environment.graphUri
