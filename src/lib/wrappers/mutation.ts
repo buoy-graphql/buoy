01d67f88908179b74e6220509b4542d93a8a4163
Albert Haff <ahn@haffdata.com>
2019-03-11 23:32:04 +0100

    WIP

--- src/lib/wrappers/mutation.ts
+++ src/lib/wrappers/mutation.ts
@@ -1,10 +1,45 @@
-import { Apollo } from 'apollo-angular';
 import { MutationOptions } from './options';
+import { Wrapper } from './wrapper';
+import { Buoy } from '../buoy';
 
-export class Mutation {
-    private _query;
+export class Mutation extends Wrapper {
+    private _mutation;
 
-    constructor(private apollo: Apollo, query, options: MutationOptions) {
+    /**
+     * Is the mutation ready?
+     */
+    public _initialized = false;
 
+    /**
+     * Contains the scoped response.
+     */
+    public data: any;
+
+    /**
+     * Is the mutation loading?
+     */
+    public loading = true;
+
+    constructor(buoy: Buoy, id: number, mutation, public _variables, protected _options: MutationOptions) {
+        super(buoy, id, 'query');
+        this.debug('debug', 'Initializing Query...');
+
+        if (_options.fetch !== false) {
+            this.initMutation(mutation);
+        }
+
+        return this;
+    }
+
+    private initMutation(mutation) {
+        this._mutation = this._buoy.apollo.mutate({
+            mutation: mutation,
+            variables: this.variables
+        });
+        this._initialized = true;
+    }
+
+    private get variables() {
+        return [];
     }
 }
