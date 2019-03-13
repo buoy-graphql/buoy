654448dd163f88fce17cfa0fe383d4368d69d588
Albert Haff <ahn@haffdata.com>
2019-03-13 16:45:19 +0100

    lint

--- example/src/app/app.component.ts
+++ example/src/app/app.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
 import gql from 'graphql-tag';
-import {Buoy} from '../../../src/lib/buoy';
-import {Query} from '../../../src/lib/wrappers/query';
-import {QueryOptions} from '../../../src/lib/wrappers/options';
+import { Buoy } from '../../../src/lib/buoy';
+import { Query } from '../../../src/lib/wrappers/query';
+import { QueryOptions } from '../../../src/lib/wrappers/options';
 
 @Component({
   selector: 'app-root',
